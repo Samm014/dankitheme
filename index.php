3c21d03e2e02b584fa0472b1a9dc80292499023f
Samuel Medeiros de Albuquerque <samuel.medeiros014@gmail.com>
2022-12-20 11:22:33 -0300

    commit initial

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danki Theme - Theme for Wordpress</title>
+    <!---->
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+    <!---->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="js/slide.js"></script>
+<body>
+    <section class='header'>
+        <div class="center">
+            <header>
+                <div class="logo">
+                    <img src="images/logo.png" alt="" srcset="">
+                </div><!--logo-->
+                <ul class="menu">
+                    <li><a href="">Início</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </header>
+            <main>
+                <div class="infos">
+                    <h2>Melhore a comunicação com seu cliente e time.</h2>
+                    <p>Consultoria especializada em startups, empresas e 
+                    principalmente, pessoas.</p>
+                    <a class="buttonDemonstracao" href="">Ver Demonstração</a>
+                </div><!--infos-->
+                <div class="ilustra1">
+                    <img src="images/ilustra1.png" alt="">
+                </div><!--ilustre1-->
+            </main>
+        </div><!--center-->
+    </section><!--header-->
+
+    <section class="clientSlide">
+        <div class="buttonDown">
+            <i class="fa-solid fa-angle-down"></i>
+        </div><!--buttonDown-->
+            <div id="slide" class="slide">
+                <img src="images/clientLogo/amazon.png" alt="" srcset="">
+                <img src="images/clientLogo/costco.png" alt="" srcset="">
+                <img src="images/clientLogo/dominos.png" alt="" srcset="">
+                <img src="images/clientLogo/uber.png" alt="" srcset="">
+                <img src="images/clientLogo/walmart.png" alt="" srcset="">
+            </div><!--slide-->
+    </section><!--clientSlide-->
+
+    <section class="contribu">
+        <div class="center">
+            <h2>Contribuímos de ponta a ponta</h2>
+            <div class="boxs">
+
+                <div class="box">
+                    <img src="images/ilustracao-01.png" alt="" srcset="">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+                </div><!--box-->
+
+                <div class="box">
+                    <img src="images/ilustracao-02.png" alt="" srcset="">
+                    <h3>Sites e Intranets</h3>
+                    <p>Um diálogo só acontece quando ambas as partes falam e escutam, garanta que seu pública te entende.</p>
+                </div><!--box-->
+
+                <div class="box">
+                    <img src="images/ilustracao-03.png" alt="" srcset="">
+                    <h3>Comunicação one-to-one</h3>
+                    <p>O digital veio pra ficar, sem dúvidas, mas uma comunicação integrada de verdade exige clarezas nas descisões.</p>
+                </div><!--box-->
+
+            </div><!--boxs-->
+        </div><!--center-->
+    </section><!--contribu-->
+</body>
+</html>
---
+++ js/slide.js
@@ -0,0 +1,35 @@
+$(function(){
+    $('#slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+    ]
+  });
+})
